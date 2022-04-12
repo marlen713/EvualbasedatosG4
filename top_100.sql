f540203632bd17f860ff1b7f9ac9672d3d9314bf
marlen713 <ma000161@gmail.com>
2022-04-12 19:55:17 -0400

    tercer commit

--- top_100.sql
+++ top_100.sql
@@ -22,3 +22,16 @@ CREATE TABLE reparto(
 SELECT COUNT(id) AS Nropeliculas FROM peliculas; --
 SELECT COUNT(peliculasId) AS Nroreparto FROM reparto; 
 
+--Obtener el ID de la película “Titanic”. 
+SELECT id FROM peliculas WHERE pelicula = 'Titanic';
+
+--Listar a todos los actores que aparecen en la película "Titanic".
+SELECT reparto FROM reparto WHERE peliculasId = 2;
+
+--Consultar en cuántas películas del top 100 participa Harrison Ford. 
+SELECT COUNT(reparto) AS HarrisonFord FROM reparto WHERE reparto = 'Harrison Ford';
+
+--Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de manera ascendente.
+SELECT pelicula, agnoEstreno FROM peliculas WHERE agnoEstreno BETWEEN 1990 AND 1999 ORDER BY pelicula ASC;
+
+--Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser nombrado para la consulta como “longitud_titulo”.
