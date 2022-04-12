7db4a5f408a020ddb26b0a46acd20bad19d7e491
marlen713 <ma000161@gmail.com>
2022-04-12 19:26:48 -0400

    first commit

--- top_100.sql
+++ top_100.sql
@@ -10,8 +10,15 @@ CREATE TABLE peliculas(
     agnoEstreno SMALLINT, 
     director VARCHAR(30),
     PRIMARY KEY(id)
-)
+);
+\copy peliculas FROM 'peliculas.csv' csv header; --Esto me quiere decir que copie desde mi otro archivo pel.csv el titulo.
 
-
-
+CREATE TABLE reparto(
+    peliculasId INT,
+    reparto VARCHAR(40),
+    FOREIGN KEY(peliculasId) REFERENCES peliculas(id) --Une esta tabla con la  que hice en la parte de arriba llamada peliculas.
+);
+\copy reparto FROM 'reparto.csv' csv;
+SELECT COUNT(id) AS Nropeliculas FROM peliculas; --
+SELECT COUNT(peliculasId) AS Nroreparto FROM reparto; 
 
